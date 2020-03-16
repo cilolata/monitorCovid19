3764674f9d01bc034325f55c570d7d6d82f02763
cilolata <cilolata@gmail.com>
2020-03-16 15:51:44 -0300

    ajuste media querie , css border e box-shadow

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -4,10 +4,10 @@ import "./footer.css"
 export default class Footer extends Component {
     render() {
         return(
-                <div className="foo" >
+                <footer className="foo" >
                     <p className="creditos">API NEWS by <a href="https://newsapi.org/" className="creditos">Google</a></p>
                     <p className="creditos">API CoronaVírus by <a href="https://github.com/cilolata/API" className="creditos">NovelCOVID</a></p>
-                </div>
+                </footer>
         )
     }
 
