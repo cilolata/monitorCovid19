2910449c18451cfee3e7e296382a95ed8d56544f
irailtonreis <irailton.oliveira91@gmail.com>
2020-03-16 11:42:19 -0300

    Alterações footer, layout em geral e mobile

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import './App.css';
-import api from './services/api';
 import Main from './pages/main';
 import Noticias from './pages/noticias';
 import Footer from './pages/footer';
 
 function App() {
   return (
+    <>
     <div className="App">
       <Main />
       <Noticias />
-      <div className="footer">
-      <Footer />
-
-      </div>
     </div>
+      <Footer />
+    </>
+
   );
 }
 
