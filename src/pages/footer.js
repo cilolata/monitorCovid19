f124135bfbd2e9b1eb71295188daa35a3ac97fc5
almeidarenato <renatoam@ymail.com>
2020-03-17 21:27:35 -0300

    Revert "added credits"
    This reverts commit 49e7e1d1a20c0ad5dbdbfcfbdd1847341fad0a81.

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -1,38 +1,15 @@
-import React, { Component } from "react";
-import "./footer.css";
+import React, { Component } from 'react';
+import "./footer.css"
 
 export default class Footer extends Component {
-  render() {
-    return (
-      <footer className="foo">
-        <p className="creditos">
-          API NEWS by{" "}
-          <a href="https://newsapi.org/" className="creditos">
-            Google
-          </a>
-        </p>
-        <p className="creditos apiCorona">
-          {" "}
-          API CoronaVírus by{" "}
-          <a href="https://github.com/cilolata/API" className="creditos">
-            NovelCOVID
-          </a>
-        </p>
-        <p className="creditos coders">
-          Coded by{" "}
-          <a href="https://github.com/cilolata" target="_blanck">
-            Ciça Lolata
-          </a>{" "}
-          ,{" "}
-          <a href="https://github.com/irailtonreis" target="_blanck">
-            Irailton Reis
-          </a>{" "}
-          and{" "}
-          <a href="https://github.com/almeidarenato" target="_blanck">
-            Renato Almeida
-          </a>
-        </p>
-      </footer>
-    );
-  }
+    render() {
+        return(
+                <footer className="foo" >
+                    <p className="creditos">API NEWS by <a href="https://newsapi.org/" className="creditos">Google</a></p>
+                    <p className="creditos apiCorona"> API CoronaVírus by <a href="https://github.com/cilolata/API" className="creditos">NovelCOVID</a></p>
+                    <p className="creditos coders">Coded by <a href="https://github.com/cilolata" target="_blanck">Ciça Lolata</a> and <a href="https://github.com/irailtonreis" target="_blanck">Irailton Reis</a></p>
+                </footer>
+        )
+    }
+
 }
